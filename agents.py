da8ae986b66ff6e62498e77cb45d42342113d1af
Fagin-H <33319563+Fagin-H@users.noreply.github.com>
2019-06-13 18:34:23 +0100

    Added look function to agent

--- agents.py
+++ agents.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class schedule:
     
     def __init__(self, sch):
@@ -12,7 +11,7 @@ class schedule:
     def __repr__(self):
         return str(self.sch)
     
-    def curact(self, time):
+    def curact(self, time): #returns current action wanting to be done given current time
         closest = 24
         for ts in self.sch:
             if float(ts) >= time and float(ts) < float(closest):
@@ -27,10 +26,11 @@ class agent:
     
     def __init__(self, name):
         self.name = name
-        self.rs = {'self' : 1}
-        self.inven = []
-        self.home = ""
-        self.sch = schedule({})
+        self.rs = {'self' : [1]} #relationships [like]
+        self.inven = [] #inventory
+        self.locations = {} #Known locations of people and items
+        self.home = np.array([0,0]) #home location
+        self.sch = schedule({}) #schedule
         self.stats = {'str' : 10,
                       'con' : 10,
                       'dex' : 10,
@@ -38,11 +38,12 @@ class agent:
                       'wis' : 10,
                       'chr' : 10
                       }
-        self.status = {'hun' : 1,
-                       'thu' : 1,
-                       'hp' : 5,
-                       'eng' : 1,
-                       'abl' : 0,
+        self.status = {'hun' : 1, #food
+                       'thu' : 1, #water
+                       'hp' : 5, #hp
+                       'eng' : 1, #energy
+                       'abl' : 0, #alcohol
+                       'loc' : np.array([0,0]) #location
                        }
         self.desires = {}
         
@@ -53,4 +54,55 @@ class agent:
     def __repr__(self):
         return "Agent: {}".format(self.name)
     
+    def look(self, world, dist = 10):
+        for item in world['items']:
+            if np.linalg.norm(world['items'][item].loc - self.status['loc']) < dist: #finds dist between item and self, if less than maximum dist then item added to known locations
+                self.locations[item] = world['items'][item].loc
+        
+        for agent in world['agents']:
+            if np.linalg.norm(world['agents'][agent].status['loc'] - self.status['loc']) < dist: #finds dist between agent and self, if less than maximum dist then agent added to known locations
+                self.locations[agent] = world['agents'][agent].status['loc']
+        
     
+    def find_act(self):
+        pass
+    
+    def action(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
